05b2054ef607e683836f0d328fa316011465724e
Filip Palmqvist <filip.palmqvist@hitta.se>
2019-04-02 14:55:17 +0200

    Installed bootstrap

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Test extends React.Component {
     render() {
         return (
             <div>
-                <h1 style= {{color: "red"}} className="cssTest">KIIIng</h1>
+                <h1 style= {{color: "red"}} className="display-3">KIIIng</h1>
             </div>
         ); 
     }
